7cd82d925cf68642b590294d31d77cc692af48aa
Tetrapod <kzking.hrn@icloud.com>
2024-12-13 14:07:19 +0900

    口調のテスト

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,5 +1,14 @@
 import requests
 import datetime
+import json
 
 def get_weather():
     print("a")
+
+def test():
+    with open("test.json") as f:
+        data = json.load(f)
+        print(data)
+        print(data[0]["id"])
+
+test()
