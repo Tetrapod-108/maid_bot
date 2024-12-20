1838ecb807992f7afc37da02212eb8177cca1439
Tetrapod <kzking.hrn@icloud.com>
2024-12-20 14:19:13 +0900

    task.pyの中身追加 task.jsonから内容を読み取れるように

--- gemini.py
+++ gemini.py
@@ -6,24 +6,30 @@ from key import key
 
 PROMPT = \
 "長く屋敷に仕えるメイド。口調は温かみのある敬語。様々な分野に精通していて博識。主人のことを「マスター」と呼ぶ。「!」は使わない。文の最後に改行する。口調の例:「お疲れ様です、マスター。本日は気温差が大きいため、お体に気を付けてお過ごしくださいませ。」"
-CONFIG = genai.types.GenerationConfig(temperature=2.0)
+CONFIG = genai.types.GenerationConfig(temperature=1.0)
 
 def talk(msg: str, take_over_history: bool = True):
     genai.configure(api_key=key.GEMINI_API_KEY)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    history = chsl.load_gemini_history_from_json("history.json")
+    history = chsl.load_gemini_history_from_json("json/history.json")
     chat = model.start_chat(history = history)
     res = chat.send_message(content = msg, generation_config = CONFIG)
     history = chat.history
+    #print(history)
     if take_over_history == True:
-        chsl.save_gemini_history_to_json(history, "history.json")
+        chsl.save_gemini_history_to_json(history, "json/history.json")
     return res.text
+
+"""
 
 data = [{"id":0,"name":"数学課題提出", "date":"12月13日", "time":"17:00"},
         {"id":1,"name":"ミーティング", "date":"12月14日", "time":"17:00"}]
 task_str = ""
 for i in range(len(data)):
     task_str = f"{data[i]["name"]}{data[i]["date"]}{data[i]["time"]}," + task_str
-msg = f"主人に以下のようなタスクがあることを、箇条書きで伝えてください。今は12月13日13:00です。「{task_str}」"
+msg = f"「{task_str}」のようなタスクがあります。マスターへの挨拶、箇条書きで書いたタスクの一覧、気遣いの言葉、という流れでマスターに予定をリマインドしてください。今は12月13日9:00なので、適した挨拶、注意をしてください"
 print(msg)
 print(talk(msg, False))
+#print(talk("こんばんは"))
+
+"""
---
+++ task.py
@@ -0,0 +1,25 @@
+import datetime
+import json
+
+import gemini
+
+def remind_task(now_date: str):
+    with open("json/task.json") as f:
+        data = json.load(f)
+    #print(data)
+    if len(data) == 0:
+        res = gemini.talk(f"マスターへの挨拶、簡単な気遣いの言葉、という順でマスターにとくに予定がないことを通知してください。今日は{now_date}なので、適した挨拶をしてください。")
+        return res
+    task_str = ""
+    for i in range(len(data)):
+        task_str = f"{data[i]["name"]}{data[i]["date"]}{data[i]["time"]}," + task_str
+    res = gemini.talk(f"「{task_str}」のようなタスクがあります。マスターへの挨拶、箇条書きで書いたタスクの一覧、簡単な気遣いの一文、という流れでマスターに予定をリマインドしてください。今は{now_date}なので、適した挨拶、注意をしてください")
+    return res
+
+def add_task():
+    with open("json/task.json") as f:
+        data = json.load(f)
+    print(data)
+    print(data[0]["id"])
+
+print(remind_task("12月20日14:00"))
