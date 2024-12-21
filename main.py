83ee22c11a08811458a9cd263c618c8c4b4ec6c2
Tetrapod <kzking.hrn@icloud.com>
2024-12-21 09:16:10 +0900

    指定時間にタスクをリマインドするように

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 
 from key import key
 import gemini
-import weather_forecast
+#import weather_forecast
 import task
 
 # botをインスタンス化
@@ -19,19 +19,31 @@ GUILD_ID = discord.Object(id=748871577377964054)
 async def loop():
     await bot.wait_until_ready()
     
+    # タスクリストをリマインド
     now = datetime.now()
-    if 
+    if (now.hour == 21 and now.minute == 0) or (now.hour == 7 and now.minute == 0) or (now.hour == 0 and now.minute == 46):
+        msg = task.remind_task(now)
+        ch = bot.get_channel(1319690391251062835)
+        await ch.send(content=msg)
     
 
-# /test
-@tree.command(name="test",
-              description="会話のテスト",
+# /add_taskコマンド
+@tree.command(name="add_task",
+              description="タスクをリストに追加する",
               guild = GUILD_ID)
-@app_commands.describe(msg="テキスト")
-async def add_reminder_command(interaction: discord.Interaction, msg: str):
+@app_commands.describe(name="タスクの名前", date="日", time="時間")
+async def add_reminder_command(interaction: discord.Interaction, name: str, date:str = None, time:str = None):
     await interaction.response.defer()
-    res = gemini.talk(msg)
-    await interaction.followup.send(content = res, ephemeral = True)
+    format1 = r"([0-9]{1,2})/([0-9]{1,2})"
+    format2 = r"([0-9]{1,2}):([0-9]{1,2})"
+    if re.fullmatch(format1, date) == None and re.fullmatch(format2, time):
+        msg = ""
+    elif date == None or (date == None and time == None):
+        msg = gemini.talk()
+    else:
+        task.add_task(name, date, time)
+        msg = gemini.talk()
+    await interaction.followup.send(content = "aa", ephemeral = True)
 
 # メンションに反応
 @bot.event
