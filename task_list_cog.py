0f559c9529ac54524423f53e69e8fdf8e5a70366
Tetrapod <kzking.hrn@icloud.com>
2025-04-27 12:00:26 +0900

    task_list_cog.py更新

--- task_list_cog.py
+++ task_list_cog.py
@@ -3,58 +3,43 @@ from discord.ext import commands
 from discord.ext import tasks
 from discord import app_commands
 
-from features import task_list
+from features.task_list import task_repository
+import features.gemini as gemini
 
 import re
+from pathlib import Path
+
 
 class TaskListCog(commands.Cog):
     # コンストラクタ
-    def __init__(self, bot):
+    def __init__(self, bot, task_repo_file_path):
         self.bot = bot
-    
+        self.task_repo = task_repository.TaskRepository(task_repo_file_path)
+
+
     # タスクリストをリマインド
     @tasks.loop(seconds=60)
     async def remind_task_list():
         pass
 
+
     # /add_taskコマンド
     @app_commands.describe(name="タスクの名前", date="日", time="時間")
     async def add_task_command(interaction: discord.Interaction, name: str, date:str = None, time:str = None):
         await interaction.response.defer()
-        format1 = r"([0-9]{1,2})/([0-9]{1,2})"
-        format2 = r"([0-9]{1,2}):([0-9]{1,2})"
-        if date == None or time == None:
-            msg = task.add_task(name, date, time)
-        elif re.fullmatch(format1, date) == None and re.fullmatch(format2, time) == None:
-            msg = gemini.talk("マスターから受けた指示が間違っていることを伝えてください", False)
-        elif date == None or (date == None and time == None):
-            msg = task.add_task(name, date, time)
-        else:
-            msg = task.add_task(name, date, time)
-        await interaction.followup.send(content = msg, ephemeral = True)
 
 
     # /complete_taskコマンド
-    @tree.command(name="complete_task",
-                  description="完了したタスクをリストから削除する",
-                  guild=GUILD_ID)
     @app_commands.describe(name="タスクの名前")
     async def remove_task_command(interaction: discord.Interaction, name: str):
         await interaction.response.defer()
-        msg = task.remove_task(name)
-        await interaction.followup.send(content = msg, ephemeral = True)
     
     
     # /show_taskコマンド
-    @tree.command(name="show_task",
-                  description="タスクリストを表示する",
-                  guild=GUILD_ID)
     @app_commands.describe()
     async def show_task_command(interaction: discord.Interaction):
         await interaction.response.defer()
-        now = datetime.now()
-        task_list = task.remind_task(True)
-        msg = gemini.talk(f"[システム: 現在の時間:{now} 注意:各文の間に1行空ける必要はありません]「{task_list}」のようなタスクがあります。マスターへの簡単な挨拶、簡単な気遣いの一文、タスクについての簡単なまとめ、という流れでマスターに話してください。与えられた情報に適した挨拶、注意喚起をしてください。")
-        msg = "<@702791485409722388>\n" + msg + "\n\n" + task_list
-        await interaction.followup.send(content = msg, ephemeral = True)
     
+if __name__ == "__main__":
+    task_list_cog = TaskListCog(bot="bot", task_repo_file_path=f"{Path(__file__).parent}/json/task.json")
+    print(task_list_cog.task_repo.get_all())
